26cd5fbb2a8aa98b87ee7bc2fed02f53f7ec8381
Sebastian Wagner <seba.wagner@gmail.com>
2025-08-02 11:56:45 +1200

    Update player and patch methods.

--- src/main/java/org/nz/arrakeen/tennisplayerrest/PlayerController.java
+++ src/main/java/org/nz/arrakeen/tennisplayerrest/PlayerController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PlayerController {
@@ -30,4 +31,20 @@ public class PlayerController {
     public Player addPlayer(@RequestBody Player player) {
         return service.addPlayer(player);
     }
+
+    @PutMapping("/players/{id}")
+    public Player updatePlayer(@RequestBody Player player, @PathVariable int id) {
+        return service.updatePlayer(id, player);
+    }
+
+    @PatchMapping("/players/{id}")
+    public Player partialUpdate( @PathVariable int id, @RequestBody Map<String, Object> playerPatch) {
+        return service.patch(id, playerPatch);
+    }
+
+    @PatchMapping("/players/{id}/titles")
+    public void updateTitles(@PathVariable int id, @RequestBody int titles) {
+        service.updateTitles(id, titles);
+    }
+
 }
--- src/main/java/org/nz/arrakeen/tennisplayerrest/PlayerService.java
+++ src/main/java/org/nz/arrakeen/tennisplayerrest/PlayerService.java
@@ -1,10 +1,15 @@
 package org.nz.arrakeen.tennisplayerrest;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
-import java.util.List;
-import java.util.Optional;
+import jakarta.transaction.Transactional;
 
 @Service
 public class PlayerService {
@@ -46,5 +51,46 @@ public class PlayerService {
         );
         // Let JPA/Hibernate handle the ID generation
         return repo.save(player);
+    }
+
+    //method to update player
+    public Player updatePlayer(int id, Player p) {
+        //get player object by Id
+        Optional<Player> tempPlayer = repo.findById(id);
+
+        Player player = null;
+
+        //if the Optional has a value, assign it to p
+        if(tempPlayer.isPresent())
+            player = tempPlayer.get();
+
+        //update player information in database
+        player.setName(p.getName());
+        player.setNationality(p.getNationality());
+        player.setBirthDate(p.getBirthDate());
+        player.setTitles(p.getTitles());
+
+        //save updates
+        return repo.save(player);
+    }
+
+    //partial update
+    public Player patch( int id, Map<String, Object> playerPatch) {
+
+        Optional<Player> player = repo.findById(id);
+
+        if(player.isPresent()) {
+            playerPatch.forEach( (key, value) -> {
+                Field field = ReflectionUtils.findField(Player.class, key);
+                ReflectionUtils.makeAccessible(field);
+                ReflectionUtils.setField(field, player.get(), value);
+            });
+        }
+        return repo.save(player.get());
+    }
+
+    @Transactional
+    public void updateTitles(int id, int titles) {
+        repo.updateTitles(id, titles);
     }
 }
